2cf6ec96b92a2161cb25edffdc5569e175a8e17f
Foysal Ahmed Emon <emon535@gmail.com>
2019-10-18 11:41:35 +0600

    Merge Sort Implementation in Javascript half done

--- PMSCS/Algorithms/MergeSort.js
+++ PMSCS/Algorithms/MergeSort.js
@@ -10,5 +10,28 @@ function MergeSort(Data, left, right){
 
 
 function Merge(Data, left , middle, right){
-    
+    let LeftArray = Data.splice(left, middle);
+    let RightArray = Data.splice(middle, right);
+
+    LeftArray.push("999999999");
+    RightArray.push("999999999");
+
+    while(LeftArray[index]==RightArray[index]){
+
+    }
+}
+
+function compare ( A,B){
+    let  newArray = [];
+
+    A.forEach((elementOfA)=>{
+        B.forEach((elementofB)=>{
+            if(elementOfA<=elementofB){
+                newArray.push(elementOfA)
+            }
+            // else newArray.push(elementofB)
+        })
+    })
+
+    return newArray
 }
